3385a8011009d1e1050524fee667eb7f71ebe9b3
Desalegn Berihun <desunew@gmail.com>
2019-02-08 21:26:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
 
+ }
+
+ function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target')
+
+ }
+function increaseRankBy(n){
+  const incRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
+
+   for (let i = 0; i < incRank.length; i++){
+    incRank[i].innerHTML = ((parseInt(incRank[i].innerHTML)) + n)
+    }
+
+ }
+
+ function deepestChild(){
+  var deep = document.getElementById('grand-node').querySelectorAll('div')
+  return deep[deep.length-1]
+
+ }
